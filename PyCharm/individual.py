46a98df10dd70c3422a581f064a363f87e64f126
pynikcmd <ver.r762@gmail.com>
2022-10-29 01:59:56 +0300

    Add projects PyCharm

---
+++ PyCharm/individual.py
@@ -0,0 +1,7 @@
+# Вариант - 5
+
+a = int(input("Enter number: "))
+b = int(input("Enter number: "))
+c = int(input("Enter number: "))
+
+print("Volume: {0} \nSide surface area: {1}". format(a*b*c, (a+b)*2*c))
---
+++ PyCharm/numbers.py
@@ -0,0 +1,8 @@
+ch_1 = int(input("Enter number: "))
+ch_2 = int(input("Enter number: "))
+ch_3 = int(input("Enter number: "))
+ch_4 = int(input("Enter number: "))
+
+otvet = (ch_1 + ch_2) / (ch_3 + ch_4)
+
+print("Answer: %.2f"% (otvet))
